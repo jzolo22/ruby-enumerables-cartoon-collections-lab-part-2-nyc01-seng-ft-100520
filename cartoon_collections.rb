85a5cf24bca98928411a9c12ca86db32275013d3
Julia Zolotarev <jzolotarev@gmail.com>
2020-09-14 21:44:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,7 @@ end
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   planeteer_calls.find do |element|
-    element == valid_calls.each
+    element == valid_calls.include?
   end
 end
 
